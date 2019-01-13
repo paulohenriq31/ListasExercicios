982f3ce470a449c97bb919f2f60fb8af77b5dd50
unknown <ph744915@gmail.com>
2019-01-12 22:45:55 -0200

    Adição da 4ºlista junto com o primeiro exercício

---
+++ Lista 4/cotacaoDolar/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Conversão da moeda em Dolar para Real */
+
+using System;
+
+namespace cotacaoDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolar, real = 3.71, conversaoDolar;
+
+            Console.WriteLine("Este programa foi feito no dia 12/01/2018 com o valor do Dolar a R$ 3,71");
+            //Vai esperar 3 segundos para execultar o proximo codigo
+            System.Threading.Thread.Sleep(3000);
+            Console.Clear();
+
+            Console.Write("Qual a quantia em Dolar $ ");
+            dolar = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+
+            conversaoDolar = (dolar*real);
+
+            Console.Write("O valor de $ " +dolar+ " Dolar em Reais é de R$ " +conversaoDolar);
+            Console.ReadKey();
+            Console.Clear();
+
+
+
+
+
+        }
+    }
+}
