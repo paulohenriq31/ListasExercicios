f28e413aa97941118633125cf95b2d6924b33549
unknown <ph744915@gmail.com>
2019-01-13 14:59:29 -0200

    5ºExercicio

---
+++ Lista 4/aoQuadrado/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aoQuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, elevado;
+            
+            Console.WriteLine("Digite um numero");
+            valor = Convert.ToInt32(Console.ReadLine());
+
+            elevado = valor*valor;
+
+            Console.WriteLine("O valor de "+valor+" elevado ao quadradro é de "+elevado);
+            
+        }
+    }
+}
