0e5a238177f11adc30a241138b0ba9649e160ebe
unknown <ph744915@gmail.com>
2019-01-13 14:39:55 -0200

    4ºExercicio

---
+++ Lista 4/media4notas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace media4notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double nota1, nota2, nota3, nota4, mediaAluno;
+            string aluno;
+            
+            Console.Write("Escreva o nome do Aluno: ");
+            aluno = Console.ReadLine();
+
+            Console.Write("Digite a primeira nota do " + aluno +": ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digite a segunda nota do "+ aluno+ ": ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digite a terceira nota do " + aluno + ": ");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digite a quarta nota do " + aluno + ": ");
+            nota4 = Convert.ToDouble(Console.ReadLine());
+
+            mediaAluno = (nota1 + nota2 + nota3 + nota4)/4;
+            
+            Console.Write("A media da nota do aluno: " + aluno + " é: " +mediaAluno);
+        }
+    }
+}
