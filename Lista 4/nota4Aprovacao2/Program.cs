c084b11d3e06125f4045d8d0a96bd7b209c9a47c
Paulo Lan <ph744915@gmail.com>
2019-01-14 12:59:02 -0200

    10ºExercicio

---
+++ Lista 4/nota4Aprovacao2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace nota4Aprovacao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeAluno;
+            double nota1, nota2, nota3, nota4, notaMedia;
+
+            Console.Write("Qual o nome do aluno: ");
+            nomeAluno = Console.ReadLine();
+
+            Console.Write("Digite a nota do 1º bimestre do " +nomeAluno+ ": ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a nota do 2º bimestre do " +nomeAluno+ ": ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digite a nota do 3º bimestre do " +nomeAluno+ ": ");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digite a nota do 4º bimestre do " +nomeAluno+ ": ");
+            nota4 = Convert.ToDouble(Console.ReadLine());
+            
+            notaMedia = (nota1+nota2+nota3+nota4)/4;
+
+            if(notaMedia < 6){
+                Console.WriteLine("A nota do aluno " + nomeAluno+ " foi: " + notaMedia);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Voce Foi REPROVADO");
+                Console.ResetColor();
+            } else{
+                Console.WriteLine("A nota do aluno " + nomeAluno+ " foi: " + notaMedia);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Voce Foi APROVADO");
+                Console.ResetColor();
+        }
+    }
+}
