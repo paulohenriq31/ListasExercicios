b8b64352633394d0e690cb4bf83677a7d8232d10
Paulo Lan <ph744915@gmail.com>
2019-01-14 12:58:30 -0200

    11ºExercicio

---
+++ Lista 4/maior3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace maior3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n1, n2, n3;
+            Console.WriteLine("Digite 3 numeros para ver qual o maior:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            n3 = Convert.ToInt32(Console.ReadLine());
+            
+            if (n1 > n2 && n1 > n3){
+                Console.WriteLine("O maior é o " + n1);
+            } else if (n2 > n1 && n2 > n3){
+                Console.WriteLine("O maior numero é o " +n2);
+            } else if (n3 > n1 && n3 > n2){
+                Console.WriteLine("O maior numero é o " +n3);
+            } else {
+                Console.WriteLine("Os 3 numeros são iguais");
+            }
+        }
+    }
+}
