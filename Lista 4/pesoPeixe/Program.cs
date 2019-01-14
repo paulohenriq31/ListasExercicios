a6e8dd61c17ed8e205e35652fc8888b0438c38ef
Paulo Lan <ph744915@gmail.com>
2019-01-14 16:22:26 -0200

    15ºExercicio

---
+++ Lista 4/pesoPeixe/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pesoPeixe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int P/*PESO*/, E/*EXCESSO*/, M = 0/*MULTA*/; 
+            Console.Write("QUAL O PESO DO PEIXE: ");
+            P = Convert.ToInt32(Console.ReadLine());
+
+            E = (P - 50);
+
+            if (E <= 0){
+                Console.WriteLine("O peso do peixe nao ultrapassou o limite");
+                                
+            } else if (E > 0){
+                Console.WriteLine("O peso do peixe ultrapassou " +E+ "KG do limite");
+                M = (E * 4);
+                Console.WriteLine("O valor da multa sera de R$"+M+",00");
+            }
+       }
+    }
+}
