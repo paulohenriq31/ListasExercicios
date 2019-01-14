cf3e87b25be4d9ae9331e5573ff88b7260cfa20c
Paulo Lan <ph744915@gmail.com>
2019-01-14 13:13:53 -0200

    12ºExercicio

---
+++ Lista 4/positivoNegativo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace positivoNegativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            Console.WriteLine("Digite um numero");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            if (n1 < 0){
+                Console.WriteLine("O numero " +n1+ " é negativo");
+            } else if (n1 > 0){
+                Console.WriteLine("O numero " +n1+ " é positivo");
+            } else {
+                Console.WriteLine("O numero " +n1+ " é nulo");
+            }
+        }
+    }
+}
